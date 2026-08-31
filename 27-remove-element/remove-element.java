0b283f3acd4504c981ce03d7750a014e923eb558
Arif Ansari <ariffansari007@outlook.com>
2026-08-31 15:12:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.15%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,14 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int count=0;
-        int index=0;
-
+        int index =0;
         for(int i=0; i<nums.length; i++){
-           if(val!=nums[i]){
-                nums[index] = nums[i];
-                index++;
-           }
+            if(val!=nums[i]){
+                nums[index++] = nums[i];
+            }
         }
-        return index; 
+        return index;
     }
 }
