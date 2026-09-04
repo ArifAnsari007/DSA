c9832dec5260577d1b554b8822397ec68c1cd460
Arif Ansari <ariffansari007@outlook.com>
2026-09-04 10:41:22 +0530

    Time: 17 ms (80.76%) | Memory: 49.7 MB (35.53%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -2,10 +2,8 @@ class Solution {
     public int arrayPairSum(int[] arr) {
        Arrays.sort(arr);
        int Esum=0;
-       for(int i=0; i<arr.length; i++){
-         if(i%2==0){
-         Esum+=arr[i];
-         }
+       for(int i=0; i<arr.length; i+=2){
+        Esum+=arr[i];
        }
     return Esum;
     }
