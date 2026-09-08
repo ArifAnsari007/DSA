c241d9d6decb838869b826dd9afd43b2936b6b7a
Arif Ansari <ariffansari007@outlook.com>
2026-09-09 00:13:48 +0530

    Time: 39 ms (37.54%) | Memory: 47.1 MB (40.99%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+      int n = arr.length;
+      for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            int sum = arr[i]+arr[j];
+            if(sum==target){
+                return new int[]{i,j};
+            }
+        }
+      }
+      return new int[]{};
+    }
+}
